7aed6b981c6a6769fbdd812b250d21405870c9ac
mjkwebdev <khanmona17321@gmail.com>
2026-01-02 04:40:33 +0500

    Implement Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import {  Grid, GridItem } from "@chakra-ui/react";
+import {  Grid, GridItem, Theme } from "@chakra-ui/react";
 import Navbar from "./comp/Navbar";
 function App() {
   return (
+
 <Grid 
 templateAreas={{
   base: `"nav" "main"`,
   lg: `"nav nav" "aside main"`
-  }}
-  >
+}}
+>
   <GridItem area={"nav"}> <Navbar/> </GridItem>
   <GridItem display={{base:"none", lg:"block"}} area={"aside"} bg="pink">Aside</GridItem>
   <GridItem area={"main"} bg="dodgerblue"> Main</GridItem>
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider, defaultSystem, Theme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -6,7 +6,9 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <App />
+    <Theme appearance="dark">
+       <App />
+      </Theme> 
     </ChakraProvider>
   </React.StrictMode>
 );
