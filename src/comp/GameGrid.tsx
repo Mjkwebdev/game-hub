12cf2f6c11ffb58725c0906c54b88abdc2e7255a
mjkwebdev <khanmona17321@gmail.com>
2026-01-04 02:46:24 +0500

    Building Cards

--- src/comp/GameGrid.tsx
+++ src/comp/GameGrid.tsx
@@ -1,21 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Text } from '@chakra-ui/react';
-import apiClient from 'src/services/api-client';
-import useGames from 'src/hook/hook';
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "src/hook/hook";
+import GameCard from "./GameCard";
 const GameGrid = () => {
-     const {games,error}= useGames()
-    return (
-        <>
-        
-        <div> 
-         {error &&   <Text> {error}</Text>}
-  <ul>
-    {games.map(game =>
-    <li key={game.id}>{game.name}</li>)}
-  </ul>
-    </div>
+  const { games, error } = useGames();
+  return (
+    <>
+      <div>
+        {error && <Text> {error}</Text>}
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+          padding="10px"
+          spaceX={8}
+          spaceY={8}
+        >
+          {games.map((game) => (
+            <GameCard key={game.id} game={game} />
+          ))}
+        </SimpleGrid>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
--- src/hook/hook.ts
+++ src/hook/hook.ts
@@ -2,9 +2,10 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "src/services/api-client";
 
-interface Game{
+export interface Game{
     id:number;
     name: string;
+    background_image:string;
 }
 
 interface FetchGameResponse{
